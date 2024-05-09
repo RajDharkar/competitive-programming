563761e6755888783ceca2879d92f3fdbae570b5
Amoyzzz <dharkarajveer@gmail.com>
2024-05-08 22:03:05 -0400

    push
    push

---
+++ Contests/AtCoder/352/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  string s, t;
+  cin >> s >> t;
+  int n = int(s.size());
+  int m = int(t.size());
+  int ptr = 0;
+  for (int i = 0; i < n; i++) {
+    while (ptr < m && t[ptr] != s[i]) {
+      ptr += 1;
+    }
+    assert(ptr < m);
+    ptr += 1;
+    cout << ptr << " \n"[i == n - 1];
+  }
+  return 0;
+}
