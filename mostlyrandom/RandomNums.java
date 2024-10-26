95d1ebd77166b8b69a19e007f6c5d93e87471240
Amoyzzz <dharkarajveer@gmail.com>
2024-10-25 22:59:50 -0400

    stuff
    stuff

---
+++ mostlyrandom/RandomNums.java
@@ -0,0 +1,30 @@
+package Projects;
+import java.util.Scanner;
+
+public class RandomNums {
+    public static void main(String[] args) throws InterruptedException {
+        String k = "";
+        Scanner s = new Scanner(System.in);
+        while (!k.equals("stop")) {
+            System.out.println("Generating Numbers");
+
+            for (int i = 0; i < 15; i++) {
+                System.out.print(".");
+                Thread.sleep(500);
+            }
+            System.out.println("");
+
+            int x = (int) (Math.random() * 20) + 1;
+            int y = (int) (Math.random() * 20) + 1;
+            System.out.println(x + " " + y);
+            System.out.println("\n\n");
+
+            k = s.nextLine();
+            if (k.equals("y")) {
+                System.out.println(x * y);
+            }
+            System.out.println("-------------------------");
+        }
+        s.close();
+    }
+}
