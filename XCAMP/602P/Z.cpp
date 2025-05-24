dcf96a22de99dfbb2703ec16dbb7a260cacdcc3d
Amoyzzz <dharkarajveer@gmail.com>
2025-05-24 03:20:47 -0400

    Z... learn ev indicators

---
+++ XCAMP/602P/Z.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pii;
+
+#define sz(x) ll((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define pb push_back
+#define f first
+#define s second
+
+const ll MOD = 988244353
+int main(){
+    ll n, m, k; cin >> n >> m >> k;
+
+}
