90eb51a5ee34b383e25a5d22eaf41a177e765d09
Amoyzzz <dharkarajveer@gmail.com>
2025-06-24 02:14:49 -0400

    atcoder stuff

--- AtCoder/dp/C_Vacation.cpp
+++ AtCoder/dp/C_Vacation.cpp
@@ -14,9 +14,28 @@ typedef pair<ll, ll> pii;
 #define vi vector<ll>
 #define vvi vector<vi>
 #define res(x, n) (x).resize(n)
-
 void solve(){
-    
+    fastio;
+    int n;
+    cin >> n;
+    vvi dp(n+1, vi(3));
+    for(int i = 1; i <= n; i++){
+        //DP[i][0,1,2] = the maximum amount of money that we will gain if we CHOOSE to take 0, 1, 2
+        for(int c = 0; c < 3; c++){
+            int x;
+            cin >> x;
+            for(int other = 0; other < 3; other++){
+                if(other != c){
+                    dp[i][c] = max(dp[i][c], dp[i - 1][other] + x);
+                }
+            }
+        }
+    }
+    ll mx = 0;
+    for(int i = 0; i < 3; i++){
+        mx = max(mx, dp[n][i]);
+    }
+    cout << mx << '\n';
 }
 
 int main(){
